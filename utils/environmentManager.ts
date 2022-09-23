b3f80fe719906ada34e37c0d6b6fda8079e5da6e
augericke <augericke@gmail.com>
2022-09-23 14:51:06 -0400

    add env manager

---
+++ utils/environmentManager.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+function isDev(): boolean {
+  return process.env.NODE_ENV === "development";
+}
+
+function isProd(): boolean {
+  return process.env.NODE_ENV === "production";
+}
+
+const api = axios.create({
+  baseURL: `${
+    isDev() ? "http://localhost:3001/api/" : "http://localhost:3001/api/"
+  }`,
+});
+
+export { isDev, isProd, api };
