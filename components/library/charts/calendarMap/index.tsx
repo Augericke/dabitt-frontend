ed38822c9166e8eff42b9518d87c55fbe1e9e9bb
augericke <augericke@gmail.com>
2022-10-04 23:49:49 -0400

    add chart color styling

--- components/library/charts/calendarMap/index.tsx
+++ components/library/charts/calendarMap/index.tsx
@@ -10,24 +10,75 @@ type CalenderMapProps = {};
 
 const calData: any = [
   {
+    day: "2016-07-18",
+    value: 222,
+  },
+  {
+    day: "2016-07-19",
+    value: 130,
+  },
+  {
+    day: "2016-07-20",
+    value: 452,
+  },
+  {
     day: "2016-07-21",
     value: 200,
   },
+  {
+    day: "2016-07-22",
+    value: 100,
+  },
+  {
+    day: "2016-07-23",
+    value: 400,
+  },
+  {
+    day: "2016-07-24",
+    value: 15,
+  },
+  {
+    day: "2016-07-25",
+    value: 220,
+  },
 ];
+
+function addAlpha(color: string, opacity: number) {
+  // coerce values so ti is between 0 and 1.
+  var _opacity = Math.round(Math.min(Math.max(opacity || 1, 0), 1) * 255);
+  return color + _opacity.toString(16).toUpperCase();
+}
+
+const defaultColors = [
+  variable["chart-color-lowest"],
+  variable["chart-color-low"],
+  variable["chart-color-medium"],
+  variable["chart-color-high"],
+  variable["chart-color-highest"],
+];
+
+const opacityRange = [0.1, 0.3, 0.5, 0.7, 1];
+const colors = opacityRange.map((opacity) => {
+  return addAlpha(variable["category-color-forest"], opacity);
+});
+console.log(colors);
 
 const CalenderMap: React.FC<CalenderMapProps> = (props: CalenderMapProps) => {
   return (
-    <div style={{ width: "100%", height: "300px" }}>
+    <div style={{ width: "100%", height: "400px" }}>
       <ResponsiveTimeRange
         data={calData}
-        from="2016-07-01"
-        to="2016-09-29"
+        from="2016-02-01"
+        to="2016-7-31"
+        theme={{
+          textColor: variable["text-color"],
+        }}
         emptyColor={variable["background-color"]}
-        colors={[variable["category-color-default"]]}
+        colors={colors}
         margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
-        dayBorderWidth={1}
+        dayBorderWidth={0.5}
         daySpacing={5}
-        dayBorderColor={variable["category-color-default"]}
+        dayBorderColor={colors[2]}
       />
     </div>
   );
