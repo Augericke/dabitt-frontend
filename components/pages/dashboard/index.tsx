d79df3946f6cf94ae511cb917d081372d345ab87
augericke <augericke@gmail.com>
2022-10-20 12:01:52 -0400

    add skelly

--- components/pages/dashboard/index.tsx
+++ components/pages/dashboard/index.tsx
@@ -4,10 +4,15 @@ import { motion } from "framer-motion";
 import { getSelectableColorClass } from "../../../utils/selectableColorClass";
 import { CategoryModel } from "../../../types/category";
 import { IconColors } from "../../../types/task";
+import colors from "../../../styles/_selectableColors.module.scss";
 import BarChart from "../../library/charts/bar";
 import { useCalendarCompleted } from "../../../utils/hooks/query/analytics/useCalendarCompleted";
 import ShowOnViewport from "../../library/animation/showOnViewport";
 import { useWeekCompleted } from "../../../utils/hooks/query/analytics/useWeekCompleted";
+
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
 const styles = require("./dashboard.module.scss");
 
 type DashboardViewProps = {
@@ -62,7 +67,12 @@ const DashboardView: React.FC<DashboardViewProps> = ({ categories }) => {
       </div>
       <ShowOnViewport customClass={styles.barContainer}>
         {weekData.isLoading ? (
-          <></>
+          <SkeletonTheme
+            baseColor={colors["foreground-color"]}
+            highlightColor={colors["icon-color"]}
+          >
+            <Skeleton width="100vw" height="100vh" />
+          </SkeletonTheme>
         ) : weekData.isError ? (
           <></>
         ) : (
@@ -77,7 +87,14 @@ const DashboardView: React.FC<DashboardViewProps> = ({ categories }) => {
       </ShowOnViewport>
       <ShowOnViewport customClass={styles.calenderContainer}>
         {calendarData.isLoading ? (
-          <></>
+          <SkeletonTheme
+            baseColor={colors["foreground-color"]}
+            highlightColor={colors["icon-color"]}
+          >
+            <div className={styles.calenderSkelton}>
+              <Skeleton width="100vw" height="100vh" />
+            </div>
+          </SkeletonTheme>
         ) : calendarData.isError ? (
           <></>
         ) : (
